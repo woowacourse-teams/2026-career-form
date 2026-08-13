3cf941ab123628d137d182f76c877d171993b8d8
picetea44 <picetea44@gmail.com>
2026-08-13 22:37:25 +0900

    build: 백엔드 품질 도구 기준 정합화

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "4.1.0"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "7.4.0.8496" apply false
 }
 
 group = "com.careerform"
@@ -63,5 +64,5 @@ tasks.jacocoTestCoverageVerification {
 }
 
 tasks.check {
-    dependsOn(tasks.jacocoTestCoverageVerification)
+    dependsOn(tasks.jacocoTestReport, tasks.jacocoTestCoverageVerification)
 }
